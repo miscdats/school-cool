b68c22749ee574c26431fb7586eac471adc54994
“stuick” <stuick@gmail.com>
2019-01-27 15:03:39 -0800

    creating the basic layers for all pages. common changes will be in all.css and specific changes will have <page name>.css except index that will be login

---
+++ app/index.php
@@ -0,0 +1,33 @@
+<link rel="stylesheet" href="CSS/all.css" type="text/css">
+<link rel="stylesheet" href="css/login.css" type="text/css">
+
+<html>
+    <head>
+        <title>School Cool Login</title>
+    </head>
+    <body>
+        <header>
+            
+        </header>
+        
+        <div id="wrapper">
+            
+            <nav>
+            
+            </nav>
+            
+            <main>
+                
+            </main>
+            
+            <aside>
+            
+            </aside>
+            
+            <footer>
+            
+            </footer>
+        </div>
+    </body>
+</html>
+

46b300b61ea3470006385d1eaf106f8fdfd7471e
“stuick” <stuick@gmail.com>
2019-01-27 16:37:36 -0800

    These pages will be the grade page and the home page

---
+++ app/home.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="CSS/all.css" type="text/css">
+<link rel="stylesheet" href="css/login.css" type="text/css">
+
+<html>
+    <head>
+        <title>School Cool Login</title>
+    </head>
+    <body>
+        <header>
+
+        </header>
+
+        <div id="wrapper">
+
+            <nav>
+
+            </nav>
+
+            <main>
+
+            </main>
+
+            <aside>
+
+            </aside>
+
+            <footer>
+
+            </footer>
+        </div>
+    </body>
+</html>

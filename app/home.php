b9bc8b55a3f6d165de7f98ccad6f6966672a2e4c
“stuick” <stuick@gmail.com>
2019-01-27 23:04:04 -0800

    added some more php

--- app/home.php
+++ app/home.php
@@ -1,32 +1,52 @@
 <link rel="stylesheet" href="CSS/all.css" type="text/css">
-<link rel="stylesheet" href="css/login.css" type="text/css">
+<link rel="stylesheet" href="" type="text/css">
 
 <html>
     <head>
-        <title>School Cool Login</title>
+        <title>School Cool</title>
     </head>
     <body>
-        <header>
+        
+		<?php
+			function head(){
+				echo"<header> <h1>SCOOL COOL TRACKER</h1>
 
-        </header>
+        </header>";
+			}
+			head();
+			
+		?>
+		
 
         <div id="wrapper">
+			<?php
+				function nav(){
+				echo"<nav><ul><li><a href=\"home.php\">HOME</a></li><li><a href=\"classes.php\">CLASSES</a></li><li><a href=\"grades.php\">GRADES</a></li><li><a href=\"ressources.php\">RESSOURCES</a></li></ul></nav>";
+				}
+				nav();
+			?>
 
-            <nav>
-
-            </nav>
+            
 
             <main>
 
             </main>
 
             <aside>
+			
+			</aside>
 
-            </aside>
+			<?php
+			function foot(){
+				echo"<footer><h2>The footer is here</h2>
 
-            <footer>
+            </footer>";
+			}
+			foot();
+			
+		?>
 
-            </footer>
+            
         </div>
     </body>
 </html>

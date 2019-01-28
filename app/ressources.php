de76f4be7c7c520ae3def461461d87f244d15280
“stuick” <stuick@gmail.com>
2019-01-27 23:05:18 -0800

    created the pages for ressources and classes

---
+++ app/ressources.php
@@ -0,0 +1,38 @@
+<?php
+	require('home.php')
+?>
+
+<html>
+    <head>
+        <title>School Cool Ressources</title>
+    </head>
+    <body>
+        
+		<?php
+			head();			
+		?>
+		
+
+        <div id="wrapper">
+			<?php
+				nav();
+			?>
+
+            
+
+            <main>
+
+            </main>
+
+            <aside>
+			
+			</aside>
+
+			<?php
+			foot();			
+		?>
+
+            
+        </div>
+    </body>
+</html>

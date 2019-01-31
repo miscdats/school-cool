0ee869be5a7205acf296e8ae8ccc0403aaa599f6
deya <30326911+miscdats@users.noreply.github.com>
2019-01-30 19:56:07 -0800

    update grades.php - kevin

--- app/grades.php
+++ app/grades.php
@@ -1,38 +1,68 @@
 <?php
-	require('content/content.php')
+    require('content.php')
 ?>
 
 <html>
-    <head>
-        <title>School Cool :: Grades</title>
-    </head>
-    <body>
+   <head>
+       <title>School Cool gradesx</title>
+   </head>
+   <body>
 
-		<?php
-			head();
-		?>
-
-
-        <div id="wrapper">
-			<?php
-				nav();
-			?>
+        <?php
+            head();            
+        ?>
 
 
-
-            <main>
-
-            </main>
-
-            <aside>
-
-			</aside>
-
-			<?php
-			foot();
-		?>
+       <div id="wrapper">
+            <?php
+                nav();
+            ?>
 
 
-        </div>
-    </body>
+
+           <main>
+               <div id="table"><!--table div-->
+                   <table id="grades">
+                       <tr>
+
+                           <th>Grade </th>
+                           <th> Class</th>
+
+                       </tr>
+                       <tr>
+
+                           <td> <input type="text" name="grade1" placeholder="Grade..."></td>
+                           <td> <input type="text" name="class1"placeholder="Class..."></td>
+                       </tr>
+
+                   </table>
+                   <input type="submit" value=Submit>
+               </div> <!-- end of table div-->
+
+               <div id="tableform"><!-- submitted form div-->
+                   <table id="submitted">
+                       <tr>
+                           <th>Grade </th>
+                           <th> Class</th>
+                       </tr>
+                       <td>Will retrieve mysql data and display here row 1 cell 1 </td>
+                       <td>Will retrieve mysql data and display here row 1 cell 2 </td>
+                   </table>
+               </div><!--end of submitted form div-->
+           </main>
+
+           <aside>
+               <div id="search">
+                   <input type=text" name="search" placeholder="Search..">
+
+               </div>
+            </aside>
+
+            <?php
+            foot();            
+        ?>
+
+
+       </div>
+   </body>
 </html>

eefba61c6e702004b8351e07b10bd3a9d6b55cdc
deyanira <30326911+miscdats@users.noreply.github.com>
2019-01-30 20:10:31 -0800

    playing with login forms

---
+++ app/index2.php
@@ -0,0 +1,30 @@
+<?php require('content/content2.php')
+?>
+<link rel="stylesheet" href="css/login2.css" type="text/css">
+
+<html>
+    <head>
+        <title>School Cool :: Login</title>
+    </head>
+    <body>
+        <?php
+			head();
+		?>
+
+        <div id="wrapper">
+
+            <main>
+                <?php
+                    login();
+                ?>
+
+
+            </main>
+
+			<?php
+				foot();
+			?>
+
+        </div>
+    </body>
+</html>

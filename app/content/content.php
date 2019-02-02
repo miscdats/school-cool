9d73304833e87ebba4b7c9122c11e0cff0d4dc9c
“stuick” <stuick@gmail.com>
2019-02-02 14:06:52 -0800

    Fix the navigation bug

--- app/content/content.php
+++ app/content/content.php
@@ -4,10 +4,10 @@
 		echo "<nav>
 				<ul>
 					<li><a href=\"home.php\">HOME</a></li>
-					<li><a href=\"contact.php\">CONTACT US</a></li>
 					<li><a href=\"grades.php\">GRADES</a></li>
 					<li><a href=\"resources.php\">RESOURCES</a></li>
-						<div class=\"dropdown\">
+                    
+						<li><div class=\"dropdown\">
 							<button class=\"dropbtn\">
 								<li><a href=\"classes.php\">CLASSES</a>
 								<i class=\"fa fa-caret-down\"></i></li>
@@ -17,7 +17,9 @@
 								  <a href=\"assignments.php\">Assignments</a>
 								  <a href=\"goals.php\">My Goals</a>
 							</div>
-						</div>
+						</div></li>
+                        
+                        <li><a href=\"contact.php\">CONTACT US</a></li>
 				</ul>
 			</nav>";
 		}
